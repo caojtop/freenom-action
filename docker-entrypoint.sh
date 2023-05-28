647d1ee2d198f32c48aa3e8e9561544f0481414b
odoo cabin <306852160@qq.com>
2023-05-28 22:18:52 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,7 +21,7 @@ fi
 #lsb_release -a
 #ls -all /etc/netplan
 uname -a
-
+cat /etc/resolv.conf
 grep -n "FREENOM_USERNAME=" /conf/.env | cut -f1 -d: | xargs -I {} sed -i "{}s/.*/FREENOM_USERNAME='$1'/" /conf/.env
 grep -n "FREENOM_PASSWORD=" /conf/.env | cut -f1 -d: | xargs -I {} sed -i "{}s/.*/FREENOM_PASSWORD='$2'/" /conf/.env
 export http_proxy=http://cj:cj123@y1.honor.tk:12321
